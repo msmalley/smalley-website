de9062a2b307e79be8750dceeb066d9b26065436
Mark Smalley <mark@smalley.my>
2026-06-01 16:04:38 +0100

    Bump to 1.0.21

--- js/sm-loader.js
+++ js/sm-loader.js
@@ -1,5 +1,5 @@
 (function() {
-  var V = '1.0.20';
+  var V = '1.0.21';
   var scripts = document.querySelectorAll('script[src*="sm-loader.js"]');
   var src = scripts[scripts.length - 1].getAttribute('src');
   var base = src.replace(/sm-loader\.js.*$/, '');
